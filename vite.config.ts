ac93845605784e39dc182e3462ab660c0a896e27
uzmdrufukkarbas <uzmdrufukkarbas@gmail.com>
2026-01-07 01:43:06 +0300

    Add server configuration to vite.config.ts
    Configure Vite server settings for external access.

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true, // Sunucunun dışarıdan erişilebilir olmasını sağlar (Container/Cloud için)
+    strictPort: true,
+    port: 5173
+  }
 })
